6563448744f594d61db76df466fca1b195b5a6af
Raquel Neves <raquelneves.design@gmail.com>
2025-11-20 15:12:41 +0000

    Initial commit

---
+++ src/components/all/Navigation.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+function Navigation() {
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/films">Films</Link>
+      <Link to="/about">About</Link>
+    </nav>
+  )
+}
+
+export default Navigation
