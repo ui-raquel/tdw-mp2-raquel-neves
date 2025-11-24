136e325f735f746dbf502a47b43d704f21a08778
Raquel Neves <raquelneves.design@gmail.com>
2025-11-24 10:53:36 +0000

    feat: update TMDB API base URL for correct endpoint access

--- src/store/api/tmdbApi.js
+++ src/store/api/tmdbApi.js
@@ -3,13 +3,14 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://api.themoviedb.org/3/authentication",
+    baseUrl: "https://api.themoviedb.org/3/",
     prepareHeaders: (headers) => {
       headers.set("Authorization", `Bearer ${import.meta.env.VITE_TMDB_READ_TOKEN}`);
       headers.set("accept", "application/json");
       return headers;
     },
   }),
+  
   tagTypes: ["MovieSearch", "MovieImages", "MovieProviders"],
   endpoints: (builder) => ({
 
